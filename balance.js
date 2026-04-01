c73697ed34fbed07ad01dbe924906280d73ce269
Гаврилюк Софія <sofiia.havryliuk@nure.ua>
2026-04-01 14:42:40 +0300

    feat: додано баланс і поповнення

---
+++ balance.js
@@ -0,0 +1,17 @@
+const { users } = require("./auth");
+
+function showBalance(username) {
+    console.log(Баланс: ${users[username].balance} грн);
+}
+
+function depositBalance(username, amount) {
+    if (amount > 0) {
+        users[username].balance += amount;
+        console.log("Баланс поповнено.");
+        console.log(Новий баланс: ${users[username].balance} грн);
+    } else {
+        console.log("Невірна сума.");
+    }
+}
+
+module.exports = { showBalance, depositBalance };
