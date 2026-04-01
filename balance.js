232f6c4c402111f67717ec771e1c72cb6d41596a
Кризька Іванна <ivanna.kryzka@nure.ua>
2026-04-01 14:49:22 +0300

    feat: виправлено синтаксичні  помилки

--- balance.js
+++ balance.js
@@ -1,14 +1,14 @@
 const { users } = require("./auth");
 
 function showBalance(username) {
-    console.log(Баланс: ${users[username].balance} грн);
+    console.log(`Баланс: ${users[username].balance} грн`);
 }
 
 function depositBalance(username, amount) {
     if (amount > 0) {
         users[username].balance += amount;
         console.log("Баланс поповнено.");
-        console.log(Новий баланс: ${users[username].balance} грн);
+        console.log(`Новий баланс: ${users[username].balance} грн`);
     } else {
         console.log("Невірна сума.");
     }
