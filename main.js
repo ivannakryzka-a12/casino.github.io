6a9dd20a7c65635b0fdf8448a42c8f2a30c2b45d
Кризька Іванна <ivanna.kryzka@nure.ua>
2026-04-01 15:00:48 +0300

    feat: оновлено main.js та додано запуск гри

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { registerUser, loginUser } = require("./auth");
 const { showBalance, depositBalance } = require("./balance");
+const { slotGame } = require("./game");
 
 let currentUser = null;
 
@@ -8,4 +9,5 @@ currentUser = loginUser("user1", "123");
 
 showBalance(currentUser);
 depositBalance(currentUser, 100);
+slotGame(currentUser, 50);
 showBalance(currentUser);
